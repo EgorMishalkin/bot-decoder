a050df332c18c7b90b1bb61701e794ea0bfb0af9
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-02-08 18:25:55 +0400

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,24 @@
+import urllib.request
+import json
+
+FOLDER_ID = "b1gjigdhd7pe26110eqg" # Идентификатор каталога
+IAM_TOKEN = "t1.9euelZqNj5yViZacmJfLnJOUjZOYne3rnpWamJGJi4uZmZ6Yxp7Hi5CUyIvl8_cODnt--e9Hbip6_N3z9048eH7570duKnr8.VQDty2" \
+            "Ypb1oQ96MwrDkmwq-Czs9FMgqadvJEL9DLuNveaGlKT2MJcq-haceEGqdXRTsdNhq1T5Ub8r2VwDZvBg" # IAM-токен
+
+with open("speech.ogg", "rb") as f:
+    data = f.read()
+
+params = "&".join([
+    "topic=general",
+    "folderId=%s" % FOLDER_ID,
+    "lang=ru-RU"
+])
+
+url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?%s" % params, data=data)
+url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
+
+responseData = urllib.request.urlopen(url).read().decode('UTF-8')
+decodedData = json.loads(responseData)
+
+if decodedData.get("error_code") is None:
+    print(decodedData.get("result"))
