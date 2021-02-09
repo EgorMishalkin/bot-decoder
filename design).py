add2a74ac49d678b4c8b496b938b2d1d5d8f53e3
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2021-02-09 17:49:23 +0400

    modern files

--- design).py
+++ design).py
@@ -3,11 +3,17 @@ from tkinter import filedialog
 from decode import decode
 from record import record, clean_file
 from pathlib import Path
+import time
 
 history = ""
 
+
 def clean_history():
-    print('working')
+    global history
+    history = ""
+    label2 = Label(text=history, justify=LEFT, background="#D3D3D3")
+    label2.place(x=10, y=50)
+
 
 
 def UploadAction(event=None):
@@ -19,15 +25,31 @@ def UploadAction(event=None):
         # проверка на тип файла
         if str(filename).split('.')[1] in ['mp3', 'ogg']:
             # запись результата в переменную и файл
-            history += "Записано! \n"
+            if text == 'format error':
+                history += "данный формат файла не подходит(длина аудио больше \n 30 секунд или слова не опознаны) \n"
+            else:
+                history += "Записано! \n"
             record(text)
         else:
             # запись в переменную ошибки
             history += 'Данный тип файла не соответствует формату mp3 или ogg( \n'
     except IndexError:
         pass
-    label2 = Label(text=history, justify=LEFT, background="#40E0D0")
+    label2 = Label(text=history, justify=LEFT, background="#D3D3D3")
     label2.place(x=10, y=50)
+
+
+def createNewWindow():
+    app = Tk()
+
+    newWindow = tk.Toplevel(app)
+    labelExample = tk.Label(newWindow, text = "New Window")
+    buttonExample = tk.Button(newWindow, text = "New Window button")
+
+    labelExample.pack()
+    buttonExample.pack()
+
+    app.mainloop()
 
 
 root = Tk()
@@ -37,16 +59,17 @@ root.resizable(width=False, height=False)
 root.iconbitmap(r'icon.ico')
 
 # background
-root["bg"] = "#40E0D0"
+root["bg"] = "#D3D3D3"
 
-label1 = Label(text='История:', justify=LEFT, font="20", background="#40E0D0")
+label1 = Label(text='История:', justify=LEFT, font="20", background="#D3D3D3")
 label1.place(x=10, y=10)
 
 btn = Button(text="Открыть", background="#0000FF", foreground="#ccc",
              padx="20", pady="8", font="16", command=UploadAction)
 btn.place(x=450, y=80, anchor="c", height=120, width=100, bordermode=OUTSIDE)
 
-btn2 = Button(text="Помощь", background="#708090", foreground="#ccc", padx="14", pady="7", font="13")
+btn2 = Button(text="Помощь", background="#2E8B57", command=createNewWindow,
+              foreground="#ccc", padx="14", pady="7", font="13")
 btn2.place(x=450, y=180, anchor="c", height=60, width=100)
 
 btn3 = Button(text="Очистить файл", background="#BA55D3",
--- record.py
+++ record.py
@@ -7,39 +7,70 @@ def clean_file():
 def record(text):
     f = open('text.txt', "a", encoding='utf8')
 
-    if text == 'формат нет':
-        f.write(f'данный формат файла не подходит(длина больше 30 секунд или слова не опознаны)')
-
-    print(text)
-    action = 1
-    text = text.lower().split(' ')
-    miss_word = ''
-    # начинаем анализировать полученный текст
-    for numb, word in enumerate(text):
-        act = False
-        if word in ['начать', 'начало', 'старт', 'овал']:
-            f.write(f'{action}) начало \n')
-            action += 1
-            act = 1
-
-        if word in ['вот', 'ввод', 'переменная', 'параллелограм', 'значение']:
-            if text[numb + 1] in ['переменной', 'переменная']:
-                f.write(f'{action}) ввод переменной {text[numb + 2]} \n')
-                action += 1
-                act = 1
-            else:
-                f.write(f'{action}) ввод переменной {text[numb + 1]} \n')
+    if text == 'format error':
+        f.write(f'данный формат файла не подходит(длина аудио больше 30 секунд или слова не опознаны)')
+    else:
+        print(text)
+        action = 1
+        text = text.lower().split(' ')
+        miss_word = ''
+        var = ['выполнить']
+        # начинаем анализировать полученный текст
+        for numb, word in enumerate(text):
+            act = False
+            if word in ['начать', 'начало', 'старт', 'овал']:
+                f.write(f'{action}) начало \n')
                 action += 1
                 act = 1
 
-        if word in ['конец', 'завершить', 'завершение', 'закончить']:
-            f.write(f'{action}) завершение схемы \n')
-            action  = 1
-            act = 1
+            if word in ['вот', 'ввод', 'переменная', 'параллелограм', 'значение', 'переменный']:
+                if text[numb + 1] in ['переменной', 'переменная']:
+                    f.write(f'{action}) ввод переменной {text[numb + 2]} \n')
+                    action += 1
+                    act = 1
+                    print(text[numb + 2])
+                    var.append(text[numb + 2])
+                else:
+                    f.write(f'{action}) ввод переменной {text[numb + 1]} \n')
+                    action += 1
+                    act = 1
+                    var.append(text[numb + 1])
 
-        if act == 0:
-            miss_word += word + ' '
 
-    f.write(f'неопознанные слова - {miss_word} \n \n')
+            if word in ['вывод', 'вывести', 'выводи']:
+                if text[numb + 1] in ['переменную', 'переменной', 'переменная']:
+                    f.write(f'{action}) вывод переменной {text[numb + 2]} \n')
+                    action += 1
+                    act = 1
+                    print(text[numb + 2])
+                    var.append(text[numb + 2])
+                else:
+                    f.write(f'{action}) вывод переменной {text[numb + 1]} \n')
+                    action += 1
+                    act = 1
+                    var.append(text[numb + 1])
 
-    f.close()
+            if word in ['конец', 'завершить', 'завершение', 'закончить']:
+                f.write(f'{action}) конец \n')
+                action  = 1
+                act = 1
+
+            if word in ['действие', 'выполнить']:
+                f.write(f'{action}) выполнить \n')
+
+            if act == 0:
+                if word in var:
+                    pass
+                else:
+                    miss_word += word + ' '
+
+        stroka = ''
+        for i in text:
+            stroka += f'{i} '
+        f.write(f'переведенный текст - {stroka} \n')
+        if miss_word != '':
+            f.write(f'неопознанные слова - {miss_word} \n \n')
+        else:
+            f.write('неопознанных слов нет \n \n')
+
+        f.close()
